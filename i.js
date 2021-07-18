cb8af6e138c972dc464796a4db2f62e233161f85
lsh <tlgud585@gmail.com>
2021-07-18 16:23:10 +0900

    add home video

--- i.js
+++
@@ -1,5 +0,0 @@
-let a ="a,b,c,d,e,f"
-a = a.split(",").map(v=>
-    "#"+v
-)
-console.log(a)
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,8 @@
 import Video from "../model/video"
 import User from "../model/user"
 export const  home = async(req,res)=>{
-    const video = await Video.find({}).populate('owner')
-    const {session : {user : {_id}}} = req
-    const user = await User.findById(_id).populate("video")
-    console.log(user)
-    res.render("home")
+    const videos = await Video.find({}).populate('owner')
+    res.render("home",{videos})
 }
 
 export const getUpload = (req,res) =>{
