44c1bad5a8fd93a91e0edb3eb601ce5c4fe820f8
lsh <tlgud585@gmail.com>
2021-08-29 17:35:30 +0900

    fix home _id

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,9 +4,11 @@ import Comment from "../model/comment"
 export const  home = async(req,res)=>{
     const  {session : {user : {_id}}}=req
     const videos = await Video.find({}).populate('owner')
-    const users = await User.findById(_id).populate({path:'subscribeVideo', populate : {path : 'owner'}})
     let subscribers =[];
-    if(users &&users.subscribeVideo.length) subscribers = users.subscribeVideo
+    if(_id){
+        const users = await User.findById(_id).populate({path:'subscribeVideo', populate : {path : 'owner'}})
+        if(users &&users.subscribeVideo.length) subscribers = users.subscribeVideo
+    }
     res.render("home",{videos,subscribers})
 }
 
